861ef2ec242db95b66901c1888200642c89729fa
German Sayago <german.sayago@gmail.com>
2024-06-10 13:38:04 -0300

    update data & styles formalities cards

--- app/ui/formalities/formalities-card.js
+++ app/ui/formalities/formalities-card.js
@@ -5,30 +5,39 @@ export default function FormalitiesCard({ formality }) {
   return (
     <div className="card">
       <div className="card-body">
-        <div className="row">
+        <div className="row justify-content-between">
           <div className="col-md-9">
             <h5 className="card-title">{formality.title}</h5>
             <p className="card-text">{formality.summary}</p>
-            <p className="card-subtitle">
-              <span>{formality.area.name}</span>
-              <span>{formality.category.name}</span>
-            </p>
+            <div className="card-detail">
+              <div>
+                <span>Responsable:</span> {formality.area.name}
+              </div>
+              <div>
+                <span>Categoría:</span> {formality.category.name}
+              </div>
+              <div>
+                <span>Modalidad:</span>{" "}
+                {formality.online == 1 ? "Online" : "Presencial"}
+              </div>
+            </div>
           </div>
           <div className="col-md-3">
             <div className="card-cta">
               <Link
                 href={`/tramites/${formality.slug}`}
-                className="btn btn-dark"
+                className="btn btn-dark mb-2"
               >
-                Mas Info
+                Ver más información
               </Link>
               <a
                 href={formality.url}
-                className="btn btn-primary text-white ms-2"
+                className={`btn btn-primary text-white ${
+                  !formality.online == 1 && "disabled opacity-25"
+                }`}
                 target="_blank"
-                disabled={!formality.url ? true : false}
               >
-                Iniciar tramite
+                Iniciar trámite online
               </a>
             </div>
           </div>
